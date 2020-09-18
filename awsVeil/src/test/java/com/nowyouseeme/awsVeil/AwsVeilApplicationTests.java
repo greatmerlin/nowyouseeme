ad9d4513457c4ce1964afdedbe32e17ec367910e
Theo <gmbaxevanos@gmx.ch>
2020-09-18 23:34:50 +0200

    spring boot project generated

---
+++ awsVeil/src/test/java/com/nowyouseeme/awsVeil/AwsVeilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowyouseeme.awsVeil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsVeilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
