510ff9863bdd6af428a707d6728a8980f1528fc9
theo <theologos.baxevanos@students.ffhs.ch>
2020-09-19 16:55:32 +0200

    added drag and drop react function

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import {useDropzone} from 'react-dropzone'
+
 
 const UserProfiles = () => {
 
@@ -20,13 +22,40 @@ const UserProfiles = () => {
 
   return UserProfiles.map((userProfile, index) => {
 
+    // for each user when we loop use the myDropzone
     return (
       <div key={index}>
+        {/* TODO: Profile Image */}
+        <br/>
+        <br/>
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileId}</p>
+        <br/>
+        <MyDropzone />
+        <br/>
       </div>
     )
   })
+}
+
+function MyDropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
 }
 
 function App() {
